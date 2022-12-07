1f5ef728d9582af42f89d0c889ea2aa714685a90
olechka27 <olechka.dobrova@gmail.com>
2022-12-07 20:56:25 +0500

    готовый обратный массив

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
+//////задача
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Массив перед :");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = temp;
+        }
+        System.out.println("\n Массив после:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 }
+
+
